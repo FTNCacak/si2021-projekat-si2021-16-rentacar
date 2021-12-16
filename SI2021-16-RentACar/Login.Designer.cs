db1a2c688c2115f036dd112da3b03a8dfbacc2e3
mmedar <71-2019@ftn.edu.rs>
2021-12-16 21:41:35 +0100

    Kreirani slojevi aplikacije.

---
+++ SI2021-16-RentACar/Login.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SI2021_16_RentACar
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.textBox2_password = new System.Windows.Forms.TextBox();
+            this.textBox1_userID = new System.Windows.Forms.TextBox();
+            this.panel1_login = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3_login = new System.Windows.Forms.Label();
+            this.label2_password = new System.Windows.Forms.Label();
+            this.label1_userID = new System.Windows.Forms.Label();
+            this.button1_login = new System.Windows.Forms.Button();
+            this.button2_register = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1_login.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2_password
+            // 
+            this.textBox2_password.BackColor = System.Drawing.Color.White;
+            this.textBox2_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.textBox2_password.Location = new System.Drawing.Point(132, 112);
+            this.textBox2_password.Name = "textBox2_password";
+            this.textBox2_password.Size = new System.Drawing.Size(220, 22);
+            this.textBox2_password.TabIndex = 13;
+            // 
+            // textBox1_userID
+            // 
+            this.textBox1_userID.BackColor = System.Drawing.Color.White;
+            this.textBox1_userID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_userID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.textBox1_userID.Location = new System.Drawing.Point(132, 75);
+            this.textBox1_userID.Name = "textBox1_userID";
+            this.textBox1_userID.Size = new System.Drawing.Size(220, 22);
+            this.textBox1_userID.TabIndex = 12;
+            this.textBox1_userID.TextChanged += new System.EventHandler(this.textBox1_userID_TextChanged);
+            // 
+            // panel1_login
+            // 
+            this.panel1_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1_login.Controls.Add(this.pictureBox1);
+            this.panel1_login.Controls.Add(this.label2);
+            this.panel1_login.Controls.Add(this.label1);
+            this.panel1_login.Location = new System.Drawing.Point(427, -5);
+            this.panel1_login.Name = "panel1_login";
+            this.panel1_login.Size = new System.Drawing.Size(394, 457);
+            this.panel1_login.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sylfaen", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(105, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 48);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Rent A Car";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label3_login
+            // 
+            this.label3_login.AutoSize = true;
+            this.label3_login.Font = new System.Drawing.Font("Sylfaen", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3_login.Location = new System.Drawing.Point(16, 107);
+            this.label3_login.Name = "label3_login";
+            this.label3_login.Size = new System.Drawing.Size(110, 27);
+            this.label3_login.TabIndex = 10;
+            this.label3_login.Text = "Password:";
+            // 
+            // label2_password
+            // 
+            this.label2_password.AutoSize = true;
+            this.label2_password.Font = new System.Drawing.Font("Sylfaen", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2_password.Location = new System.Drawing.Point(136, 9);
+            this.label2_password.Name = "label2_password";
+            this.label2_password.Size = new System.Drawing.Size(114, 48);
+            this.label2_password.TabIndex = 9;
+            this.label2_password.Text = "Login";
+            // 
+            // label1_userID
+            // 
+            this.label1_userID.AutoSize = true;
+            this.label1_userID.Font = new System.Drawing.Font("Sylfaen", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1_userID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1_userID.Location = new System.Drawing.Point(16, 70);
+            this.label1_userID.Name = "label1_userID";
+            this.label1_userID.Size = new System.Drawing.Size(94, 27);
+            this.label1_userID.TabIndex = 8;
+            this.label1_userID.Text = "User ID:";
+            // 
+            // button1_login
+            // 
+            this.button1_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1_login.Font = new System.Drawing.Font("Sylfaen", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_login.ForeColor = System.Drawing.Color.White;
+            this.button1_login.Location = new System.Drawing.Point(24, 181);
+            this.button1_login.Name = "button1_login";
+            this.button1_login.Size = new System.Drawing.Size(166, 44);
+            this.button1_login.TabIndex = 14;
+            this.button1_login.Text = "Login";
+            this.button1_login.UseVisualStyleBackColor = false;
+            // 
+            // button2_register
+            // 
+            this.button2_register.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2_register.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2_register.Font = new System.Drawing.Font("Sylfaen", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_register.ForeColor = System.Drawing.Color.White;
+            this.button2_register.Location = new System.Drawing.Point(223, 181);
+            this.button2_register.Name = "button2_register";
+            this.button2_register.Size = new System.Drawing.Size(166, 44);
+            this.button2_register.TabIndex = 15;
+            this.button2_register.Text = "Register";
+            this.button2_register.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Pristina", 84.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(79, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 147);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "AJS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(355, 139);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 410);
+            this.Controls.Add(this.button2_register);
+            this.Controls.Add(this.button1_login);
+            this.Controls.Add(this.textBox2_password);
+            this.Controls.Add(this.textBox1_userID);
+            this.Controls.Add(this.panel1_login);
+            this.Controls.Add(this.label3_login);
+            this.Controls.Add(this.label2_password);
+            this.Controls.Add(this.label1_userID);
+            this.Name = "Login";
+            this.Text = "Rent a Car";
+            this.panel1_login.ResumeLayout(false);
+            this.panel1_login.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2_password;
+        private System.Windows.Forms.TextBox textBox1_userID;
+        private System.Windows.Forms.Panel panel1_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3_login;
+        private System.Windows.Forms.Label label2_password;
+        private System.Windows.Forms.Label label1_userID;
+        private System.Windows.Forms.Button button1_login;
+        private System.Windows.Forms.Button button2_register;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ SI2021-16-RentACar/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SI2021_16_RentACar
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_userID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
