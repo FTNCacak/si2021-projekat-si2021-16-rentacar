271fdf8e09b72bd487c04476c6fab25e1455c173
unknown <jovicaantonijev00@gmail.com>
2021-12-30 00:38:23 +0100

    Povezivanje formi aplikacije.

---
+++ PresentationLayer/PresentationLayer/Menu.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace SI2021_16_RentACar
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panel1_under = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1_rent = new System.Windows.Forms.Button();
+            this.button2_tos = new System.Windows.Forms.Button();
+            this.button3_info = new System.Windows.Forms.Button();
+            this.button4_logout = new System.Windows.Forms.Button();
+            this.label3_ajs = new System.Windows.Forms.Label();
+            this.panel2_upper = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1_mainmenu = new System.Windows.Forms.Label();
+            this.label2_rentacar = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1_under.SuspendLayout();
+            this.panel2_upper.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1_under
+            // 
+            this.panel1_under.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(32)))), ((int)(((byte)(18)))));
+            this.panel1_under.Controls.Add(this.label1);
+            this.panel1_under.Location = new System.Drawing.Point(0, 489);
+            this.panel1_under.Name = "panel1_under";
+            this.panel1_under.Size = new System.Drawing.Size(935, 40);
+            this.panel1_under.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.label1.Location = new System.Drawing.Point(406, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "© 2021. AJS Soft";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1_rent
+            // 
+            this.button1_rent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(32)))), ((int)(((byte)(18)))));
+            this.button1_rent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1_rent.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_rent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.button1_rent.Location = new System.Drawing.Point(42, 105);
+            this.button1_rent.Name = "button1_rent";
+            this.button1_rent.Size = new System.Drawing.Size(166, 44);
+            this.button1_rent.TabIndex = 24;
+            this.button1_rent.Text = "Rent";
+            this.button1_rent.UseVisualStyleBackColor = false;
+            this.button1_rent.Click += new System.EventHandler(this.button2_register_Click);
+            // 
+            // button2_tos
+            // 
+            this.button2_tos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(32)))), ((int)(((byte)(18)))));
+            this.button2_tos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2_tos.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_tos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.button2_tos.Location = new System.Drawing.Point(267, 105);
+            this.button2_tos.Name = "button2_tos";
+            this.button2_tos.Size = new System.Drawing.Size(166, 44);
+            this.button2_tos.TabIndex = 25;
+            this.button2_tos.Text = "Terms of service";
+            this.button2_tos.UseVisualStyleBackColor = false;
+            // 
+            // button3_info
+            // 
+            this.button3_info.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(32)))), ((int)(((byte)(18)))));
+            this.button3_info.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3_info.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_info.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.button3_info.Location = new System.Drawing.Point(495, 105);
+            this.button3_info.Name = "button3_info";
+            this.button3_info.Size = new System.Drawing.Size(166, 44);
+            this.button3_info.TabIndex = 26;
+            this.button3_info.Text = "Info";
+            this.button3_info.UseVisualStyleBackColor = false;
+            // 
+            // button4_logout
+            // 
+            this.button4_logout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(32)))), ((int)(((byte)(18)))));
+            this.button4_logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4_logout.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4_logout.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.button4_logout.Location = new System.Drawing.Point(725, 105);
+            this.button4_logout.Name = "button4_logout";
+            this.button4_logout.Size = new System.Drawing.Size(166, 44);
+            this.button4_logout.TabIndex = 27;
+            this.button4_logout.Text = "LogOut";
+            this.button4_logout.UseVisualStyleBackColor = false;
+            // 
+            // label3_ajs
+            // 
+            this.label3_ajs.AutoSize = true;
+            this.label3_ajs.Font = new System.Drawing.Font("Pristina", 50F);
+            this.label3_ajs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.label3_ajs.Location = new System.Drawing.Point(787, 5);
+            this.label3_ajs.Name = "label3_ajs";
+            this.label3_ajs.Size = new System.Drawing.Size(145, 88);
+            this.label3_ajs.TabIndex = 28;
+            this.label3_ajs.Text = "AJS";
+            this.label3_ajs.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2_upper
+            // 
+            this.panel2_upper.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(32)))), ((int)(((byte)(18)))));
+            this.panel2_upper.Controls.Add(this.pictureBox2);
+            this.panel2_upper.Controls.Add(this.label1_mainmenu);
+            this.panel2_upper.Controls.Add(this.label2_rentacar);
+            this.panel2_upper.Controls.Add(this.label3_ajs);
+            this.panel2_upper.Location = new System.Drawing.Point(0, -1);
+            this.panel2_upper.Name = "panel2_upper";
+            this.panel2_upper.Size = new System.Drawing.Size(935, 89);
+            this.panel2_upper.TabIndex = 29;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(36, 18);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(254, 55);
+            this.pictureBox2.TabIndex = 32;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1_mainmenu
+            // 
+            this.label1_mainmenu.AutoSize = true;
+            this.label1_mainmenu.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold);
+            this.label1_mainmenu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.label1_mainmenu.Location = new System.Drawing.Point(408, 48);
+            this.label1_mainmenu.Name = "label1_mainmenu";
+            this.label1_mainmenu.Size = new System.Drawing.Size(109, 25);
+            this.label1_mainmenu.TabIndex = 31;
+            this.label1_mainmenu.Text = "Main Menu";
+            this.label1_mainmenu.Click += new System.EventHandler(this.label1_mainmenu_Click);
+            // 
+            // label2_rentacar
+            // 
+            this.label2_rentacar.AutoSize = true;
+            this.label2_rentacar.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Bold);
+            this.label2_rentacar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.label2_rentacar.Location = new System.Drawing.Point(380, 5);
+            this.label2_rentacar.Name = "label2_rentacar";
+            this.label2_rentacar.Size = new System.Drawing.Size(173, 42);
+            this.label2_rentacar.TabIndex = 30;
+            this.label2_rentacar.Text = "Rent A Car";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(210, 223);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(508, 204);
+            this.pictureBox1.TabIndex = 30;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(189)))));
+            this.ClientSize = new System.Drawing.Size(933, 524);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2_upper);
+            this.Controls.Add(this.button4_logout);
+            this.Controls.Add(this.button3_info);
+            this.Controls.Add(this.button2_tos);
+            this.Controls.Add(this.button1_rent);
+            this.Controls.Add(this.panel1_under);
+            this.Name = "Menu";
+            this.Text = "Rent a Car - AJS";
+            this.panel1_under.ResumeLayout(false);
+            this.panel1_under.PerformLayout();
+            this.panel2_upper.ResumeLayout(false);
+            this.panel2_upper.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1_under;
+        private System.Windows.Forms.Button button1_rent;
+        private System.Windows.Forms.Button button2_tos;
+        private System.Windows.Forms.Button button3_info;
+        private System.Windows.Forms.Button button4_logout;
+        private System.Windows.Forms.Label label3_ajs;
+        private System.Windows.Forms.Panel panel2_upper;
+        private System.Windows.Forms.Label label2_rentacar;
+        private System.Windows.Forms.Label label1_mainmenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
